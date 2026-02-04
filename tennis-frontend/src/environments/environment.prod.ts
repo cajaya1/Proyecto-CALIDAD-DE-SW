a58aa90dc0cbf15232a13864b8265645e90f2153
CarlosCampoverde <carlos80237@gmail.com>
2026-02-03 23:23:48 -0500

    Fix: Remove process.env from environment.prod.ts

--- tennis-frontend/src/environments/environment.prod.ts
+++ tennis-frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_URL: process.env['API_URL'] || 'https://tienda-sneakers-backend.onrender.com'
+  API_URL: 'https://tienda-sneakers-backend.onrender.com'
 };
